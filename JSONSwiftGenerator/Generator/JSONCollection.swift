6a7e2ce811a7bbe2b722653fce3fef6808e51b2c
Dan Turner <dan.turner@madebyatomicrobot.com>
2017-03-03 14:24:32 -0500

    add arrays conforming to Equatable to equatable function implementation

--- JSONSwiftGenerator/Generator/JSONCollection.swift
+++ JSONSwiftGenerator/Generator/JSONCollection.swift
@@ -82,7 +82,7 @@ extension JSONCollection: Collection {
 
 extension JSONCollection {
     var equatableItems: [(key: String, value: Element)]  {
-        return contents.filter { $0.value is Dictionary<String, Any> || $0.value is String || $0.value is Double || $0.value is Bool }
+        return contents.filter { $0.value is Dictionary<String, Any> || $0.value is String || $0.value is Double || $0.value is Bool || $0.value is [Bool] || $0.value is [String] || $0.value is [Double] }
     }
     
     var arrayItems: [(key: String, value: Element)] {
