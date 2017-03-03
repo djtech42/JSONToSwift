fda81de7bd41a2089f2f2b9922de722cb2d28192
Dan Turner <dan.turner@madebyatomicrobot.com>
2017-03-03 14:03:43 -0500

    - add specific array casting for String, Bool, and Double - create standard Swift generator functions

---
+++ JSONSwiftGenerator/SwiftLanguage.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftLanguage.swift
+//  JSONSwiftGenerator
+//
+//  Created by Dan Turner on 3/3/17.
+//  Copyright © 2017 Bren. All rights reserved.
+//
+
+import Foundation
+
+enum SwiftLanguage {
+    static func propertyString(name: String, withType type: String) -> String {
+        return "let \(name): \(type)"
+    }
+    static func initializerWithDefaultValueCast(name: String, toType type: String, defaultValueString: String) -> String {
+        return "self.\(name) = dictionary[\"\(name)\"] as? \(type) ?? \(defaultValueString)"
+    }
+    static func initializerNonOptionalCast(name: String, toType type: String) -> String {
+        return "self.\(name) = dictionary[\"\(name)\"] as \(type)"
+    }
+}
